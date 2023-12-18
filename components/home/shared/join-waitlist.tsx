c8671e8bfa5f5cfc26267f34137864a53db5af96
Samuel Chen <samuel.chen@detectorinspector.com.au>
2023-12-18 23:11:23 +1100

    feat: add join waitlist function

--- components/home/shared/join-waitlist.tsx
+++ components/home/shared/join-waitlist.tsx
@@ -1,8 +1,26 @@
+"use client"
 import SpotlightButton from "@/components/ui/spotlight-button";
 import { Mail } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import AppApi from "@/lib/apis/app-api";
+import { useState } from "react";
+import { toast } from "sonner";
+
+const appApi = new AppApi();
 
 function JoinWaitlist() {
+    const [email, setEmail] = useState("");
+
+    async function handleJoinWaitlist() {
+        const result = await appApi.subscribeEmail(email);
+
+        if (result.success) {
+            toast.success("You have successfully joined the waitlist!");
+        } else {
+            toast.error("Something went wrong due to: " + result.message);
+        }
+    }
+
     return (
         <div className="flex flex-col items-center gap-y-2">
             <div className="relative flex items-center mt-6">
@@ -13,9 +31,14 @@ function JoinWaitlist() {
                     placeholder="Your email"
                     type="email"
                     className="w-full pl-10"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    autoComplete="email"
                 />
             </div>
-            <SpotlightButton className="mt-3">
+            <SpotlightButton className="mt-3"
+                onClick={handleJoinWaitlist}
+            >
                 <span className="relative bg-gradient-to-b from-white/25 to-white bg-clip-text text-lg font-medium text-transparent transition-all duration-200 [font-variation-settings:'wdth'_100] group-hover:font-extrabold group-hover:[font-variation-settings:'wdth'_125]">
                     Join Waitlist
                 </span>
---
+++ lib/apis/app-api.ts
@@ -0,0 +1,39 @@
+import axios, {AxiosInstance} from "axios";
+
+type SubscribeEmailResponse = {
+  success: boolean;
+  message: string;
+};
+
+class AppApi {
+  private client: AxiosInstance;
+
+  constructor() {
+    this.client = axios.create({
+      baseURL: process.env.NEXT_PUBLIC_APP_API_BASE_URL,
+    });
+  }
+
+  public async subscribeEmail(email: string): Promise<{
+    success: boolean;
+    message: string;
+    error?: string;
+  }> {
+    try {
+      const response = await this.client.post<SubscribeEmailResponse>(
+        "/home/subscribe",
+        {email}
+      );
+
+      return response.data;
+    } catch (error: unknown) {
+      return {
+        success: false,
+        message: "Something went wrong",
+        error: (error as Error).message,
+      };
+    }
+  }
+}
+
+export default AppApi;
