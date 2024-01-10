ea99d4f75d60c1f7e7ff51f1364741c6efbef9f0
DbgKinggg <samuelchen0606@gmail.com>
2024-01-10 15:11:26 +1100

    fix: use meta to add og image because of vercel bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,12 +9,34 @@ import { Toaster } from 'sonner'
 import { Analytics } from '@vercel/analytics/react';
 import Navbar from '@/components/base/navbar'
 import HomeFooter from '@/components/home/footer'
-import { APP_NAME } from '@/lib/constants'
+import { APP_DESCRIPTION, APP_NAME } from '@/lib/constants'
 import { WalletProvider } from '@/components/providers/wallet-provider'
 
 export const metadata: Metadata = {
   title: APP_NAME,
-  description: `The all-in-one Web3 dashboard.`,
+  description: APP_DESCRIPTION,
+  openGraph: {
+    title: APP_NAME,
+    description: APP_DESCRIPTION,
+    images: [
+      {
+        url: 'https://burokku.one/imgaes/open-graph.png',
+        width: 2400,
+        height: 1260,
+      }
+    ]
+  },
+  twitter: {
+    title: APP_NAME,
+    description: APP_DESCRIPTION,
+    images: [
+      {
+        url: 'https://burokku.one/imgaes/open-graph.png',
+        width: 2400,
+        height: 1260,
+      }
+    ]
+  }
 }
 
 export default function RootLayout({
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,5 @@
 const APP_NAME = "Burokku";
+const APP_DESCRIPTION = "The all-in-one Web3 dashboard.";
 const CONTACT_EMAIL = "test@test.com";
 
-export {APP_NAME, CONTACT_EMAIL};
+export {APP_NAME, APP_DESCRIPTION, CONTACT_EMAIL};
