b1f3f90cb987b4c3e503f44b94ea08d16c307732
DbgKinggg <samuelchen0606@gmail.com>
2024-01-12 09:30:30 +1100

    fix: incorrect url in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,14 +3,14 @@ import {MetadataRoute} from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://https://www.burokku.one/",
-      lastModified: new Date("2024-01-10"),
+      url: "https://www.burokku.one/",
+      lastModified: new Date("2024-01-12"),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://https://www.burokku.one/terms-of-service",
-      lastModified: new Date("2024-01-10"),
+      url: "https://www.burokku.one/terms-of-service",
+      lastModified: new Date("2024-01-12"),
       changeFrequency: "yearly",
       priority: 0.8,
     },
